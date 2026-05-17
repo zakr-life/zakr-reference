a74481d5e0891203e688974b408b9c2dc6292048
ZAKR <hazeem@zakr.life>
2026-05-17 11:19:44 +0530

    Create filter_coefficients.h

---
+++ filter_coefficients.h
@@ -0,0 +1,12 @@
+#ifndef FILTER_COEFFICIENTS_H
+#define FILTER_COEFFICIENTS_H
+
+static const float lowpass_biquad[5] = {
+    0.2066f,
+    0.4131f,
+    0.2066f,
+   -0.3695f,
+    0.1958f
+};
+
+#endif
